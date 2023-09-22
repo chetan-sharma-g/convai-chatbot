2de89d6548bb796b10064d2f2d2c2ef758f52499
Chetan Sharma <66529151+chetan-sharma-g@users.noreply.github.com>
2023-09-22 23:27:37 +0530

    convai

---
+++ New folder/Convai-JS-SDK-Alpha-master/demo/chat/src/constants.ts
@@ -0,0 +1,4 @@
+export const SETTINGS: Map<string, string> = new Map([
+    ["CONVAI-API-KEY", "09ef901c3f13cf0eb026a3f99dfa35e5"],
+    ["CHARACTER-ID", "9e14971e-5313-11ee-b8c6-42010a40000b"]
+]);;
